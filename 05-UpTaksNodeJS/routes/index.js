a95a8cce5236a8d4ae687f7bbfadab0d0ec596ee
Favian Ramos <falexramos@gmail.com>
2020-05-12 19:58:53 -0500

    realizando las validaciones con express validator, eliminado caracteres espciales de los textos ingresados

--- 05-UpTaksNodeJS/routes/index.js
+++ 05-UpTaksNodeJS/routes/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+
+// importar express validator
+const {body}=require('express-validator/check');
+
+
+
+
 //importadr el controllardor
 const proyectoscontroller = require('../controllers/proyectosController');
 
@@ -10,7 +17,9 @@ module.exports = function() {
     //ruta para el home
     router.get('/', proyectoscontroller.proyectosHome);
     router.get('/nuevo-proyecto', proyectoscontroller.formularioProyecto);
-    router.post('/nuevo-proyecto', proyectoscontroller.nuevoProyecto);
+    router.post('/nuevo-proyecto', 
+    body('nombre').not().isEmpty().trim().escape(),
+    proyectoscontroller.nuevoProyecto);
 
 
 
