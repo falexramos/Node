4d1af56dfaee24194509d89d64c026bfa40d3b3d
Favian Ramos <falexramos@gmail.com>
2020-05-12 20:51:52 -0500

    mostrando los datos del objeto en helpers coomo vardump

--- 05-UpTaksNodeJS/index.js
+++ 05-UpTaksNodeJS/index.js
@@ -4,6 +4,10 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 
+//helpers con las funciones creadas
+const helpers=require('./views/helpers');
+
+
 //crear la conexión a la base de datos
 const db = require('./config/db');
 
@@ -28,6 +32,13 @@ app.set('view engine', 'pug');
 // Añadir la carpeta de las vistas
 
 app.set('views', path.join(__dirname, './views'));
+
+//pasar vardump para que se vea en toda la aplicacion
+
+app.use((rep,res,next)=>{
+    res.locals.vardump=helpers.vardump;
+    next();
+});
 
 //habilitar bodyParser para leer datos del formulario
 app.use(bodyParser.urlencoded({ extended: true }));
